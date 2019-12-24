bbeba6b2fb8c116ea5ff1f89b147094612a63f55
anand <anands9288907423@gmail.com>
2019-12-24 09:51:43 +0530

    timeline improvement

--- app/src/main/java/com/example/getmybus/MainActivity.java
+++ app/src/main/java/com/example/getmybus/MainActivity.java
@@ -1,5 +1,6 @@
 package com.example.getmybus;
 
+import androidx.annotation.NonNull;
 import androidx.annotation.Nullable;
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.recyclerview.widget.LinearLayoutManager;
@@ -20,9 +21,20 @@ import android.widget.Toast;
 
 import com.example.getmybus.Recyclerview_elements.RecyclerviewAdapter;
 import com.example.getmybus.Recyclerview_elements.TimelineData;
+import com.google.android.gms.tasks.OnCompleteListener;
+import com.google.android.gms.tasks.OnFailureListener;
+import com.google.android.gms.tasks.OnSuccessListener;
+import com.google.android.gms.tasks.Task;
+import com.google.firebase.firestore.DocumentReference;
+import com.google.firebase.firestore.DocumentSnapshot;
+import com.google.firebase.firestore.FirebaseFirestore;
+import com.google.firebase.firestore.QueryDocumentSnapshot;
+import com.google.firebase.firestore.QuerySnapshot;
 
 import java.util.ArrayList;
+import java.util.HashMap;
 import java.util.List;
+import java.util.Map;
 
 public class MainActivity extends AppCompatActivity {
     private static final String TAG = "Main activity";
@@ -33,6 +45,7 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.recycler_layout);
+        final List<TimelineData> list= new ArrayList<>();
 //        final Timeline timeline= findViewById(R.id.timeline);
 //        final EditText editText = findViewById(R.id.change_sm);
 //        editText.addTextChangedListener(new TextWatcher() {
@@ -78,18 +91,76 @@ public class MainActivity extends AppCompatActivity {
 
 //            }
 //        });
-        RecyclerView recyclerView = findViewById(R.id.recycler_view);
+
+        // Access a Cloud Firestore instance from your Activity
+        FirebaseFirestore db = FirebaseFirestore.getInstance();
+        // Create a new user with a first, middle, and last name
+        final Map<String, Object> user = new HashMap<>();
+        user.put("first", "Alan");
+        user.put("middle", "Mathison");
+        user.put("last", "Turing");
+        user.put("born", 1912);
+
+// Add a new document with a generated ID
+//        db.collection("users")
+//                .add(user)
+//                .addOnSuccessListener(new OnSuccessListener<DocumentReference>() {
+//                    @Override
+//                    public void onSuccess(DocumentReference documentReference) {
+//                        Log.d(TAG, "DocumentSnapshot added with ID: " + documentReference.getId());
+//                    }
+//                })
+//                .addOnFailureListener(new OnFailureListener() {
+//                    @Override
+//                    public void onFailure(@NonNull Exception e) {
+//                        Log.w(TAG, "Error adding document", e);
+//                    }
+//                });
+
+
+        final RecyclerView recyclerView = findViewById(R.id.recycler_view);
         LinearLayoutManager lm = new LinearLayoutManager(this,RecyclerView.VERTICAL,false);
-        List<TimelineData> list= new ArrayList<>();
-        list.add(new TimelineData("Cherthala", 100,0));
-        list.add(new TimelineData("Kvm hospital", 100,1));
-        list.add(new TimelineData("Mathilakam", 100,1));
-        list.add(new TimelineData("11th Mile", 50,2));
-        list.add(new TimelineData("bhajanamadam", 0,3));
-        list.add(new TimelineData("pallikkavala", 0,3));
-        RecyclerviewAdapter adapter = new RecyclerviewAdapter(list);
+        //TODO recycler view calculation from the database
+//      for(int i=0;i<=percent)
+//        list.add(new TimelineData("Cherthala", 100,0));
+//        list.add(new TimelineData("Kvm hospital", 100,1));
+//        list.add(new TimelineData("Mathilakam", 100,1));
+//        list.add(new TimelineData("11th Mile", 50,2));
+//        list.add(new TimelineData("bhajanamadam", 0,3));
+//        list.add(new TimelineData("pallikkavala", 0,3));
+        final RecyclerviewAdapter adapter = new RecyclerviewAdapter(list);
         recyclerView.setLayoutManager(lm);
         recyclerView.setAdapter(adapter);
+
+        // Access a Cloud Firestore instance from your Activity
+        db.collection("bus_test")
+                .get()
+                .addOnCompleteListener(new OnCompleteListener<QuerySnapshot>() {
+                    @Override
+                    public void onComplete(@NonNull Task<QuerySnapshot> task) {
+                        if (task.isSuccessful()) {
+                            for (QueryDocumentSnapshot document : task.getResult()) {
+                                Log.d(TAG, document.getId() + " => " + document.getData());
+                                document.getData().putAll(user);
+                                Log.d(TAG, "onComplete: "+document.getLong("id"));
+                                long js = document.getLong("percent");
+                                int num = Integer.parseInt(String.valueOf(js));
+                                list.clear();
+                                for(int place=0;place<num/100;place++){
+                                    if (place==0)
+                                        list.add(new TimelineData("Cherthala", 100,0));
+                                    else if (place==(num/100)-1)
+                                        list.add(new TimelineData("pallikkavala", num%100,2));
+                                    else
+                                        list.add(new TimelineData("Mathilakam", 100,1));
+                                    adapter.notifyDataSetChanged();
+                                }
+                            }
+                        } else {
+                            Log.w(TAG, "Error getting documents.", task.getException());
+                        }
+                    }
+                });
         }
 
 }
--- app/src/main/java/com/example/getmybus/MapsActivity.java
+++ app/src/main/java/com/example/getmybus/MapsActivity.java
@@ -2,8 +2,11 @@ package com.example.getmybus;
 import android.content.Context;
 import android.graphics.Bitmap;
 import android.graphics.Canvas;
+import android.graphics.drawable.BitmapDrawable;
 import android.graphics.drawable.Drawable;
+import android.nfc.Tag;
 import android.os.Bundle;
+import android.util.Log;
 
 import androidx.annotation.DrawableRes;
 import androidx.core.content.ContextCompat;
@@ -15,7 +18,9 @@ import com.google.android.gms.maps.OnMapReadyCallback;
 import com.google.android.gms.maps.SupportMapFragment;
 import com.google.android.gms.maps.model.BitmapDescriptor;
 import com.google.android.gms.maps.model.BitmapDescriptorFactory;
+import com.google.android.gms.maps.model.CameraPosition;
 import com.google.android.gms.maps.model.LatLng;
+import com.google.android.gms.maps.model.Marker;
 import com.google.android.gms.maps.model.MarkerOptions;
 
 public class MapsActivity extends FragmentActivity implements OnMapReadyCallback {
@@ -37,18 +42,39 @@ public class MapsActivity extends FragmentActivity implements OnMapReadyCallback
 
         // Add a marker in Sydney, Australia, and move the camera.
         LatLng sydney = new LatLng(-34, 151);
-        mMap.addMarker(new MarkerOptions().position(sydney).icon(bitmapDescriptorFromVector(this, R.drawable.map_pin)).title("Marker in Sydney"));
+        int height = 57;
+        int width = 43;
+        BitmapDrawable bitmapdraw=(BitmapDrawable)getResources().getDrawable(R.drawable.point_yellow);
+        Bitmap b=bitmapdraw.getBitmap();
+        Bitmap smallMarker = Bitmap.createScaledBitmap(b, width, height, false);
+        final Marker sydeney_marker = mMap.addMarker(new MarkerOptions().position(sydney).title("Marker in Sydney").icon(BitmapDescriptorFactory.fromBitmap(smallMarker)));
         mMap.moveCamera(CameraUpdateFactory.newLatLng(sydney));
+        mMap.setOnCameraMoveListener(new GoogleMap.OnCameraMoveListener() {
+            @Override
+            public void onCameraMove() {
+                CameraPosition cameraPosition = mMap.getCameraPosition();
+//                Log.d("The zoom level is", String.valueOf(cameraPosition.zoom));
+                if(cameraPosition.zoom > 10.0) {
+//                    mMap.setMapType(GoogleMap.MAP_TYPE_HYBRID);
+                    sydeney_marker.setVisible(true);
+
+                } else {
+//                    mMap.setMapType(GoogleMap.MAP_TYPE_NORMAL);
+                    sydeney_marker.setVisible(false);
+                }
+            }
+        });
     }
-    private BitmapDescriptor bitmapDescriptorFromVector(Context context, @DrawableRes int vectorDrawableResourceId) {
-        Drawable background = ContextCompat.getDrawable(context, R.drawable.pin_point);
-        background.setBounds(0, 0, background.getIntrinsicWidth(), background.getIntrinsicHeight());
-        Drawable vectorDrawable = ContextCompat.getDrawable(context, vectorDrawableResourceId);
-        vectorDrawable.setBounds(40, 20, vectorDrawable.getIntrinsicWidth() + 40, vectorDrawable.getIntrinsicHeight() + 20);
-        Bitmap bitmap = Bitmap.createBitmap(background.getIntrinsicWidth(), background.getIntrinsicHeight(), Bitmap.Config.ARGB_8888);
-        Canvas canvas = new Canvas(bitmap);
-        background.draw(canvas);
-        vectorDrawable.draw(canvas);
-        return BitmapDescriptorFactory.fromBitmap(bitmap);
-    }
+
+//    private BitmapDescriptor bitmapDescriptorFromVector(Context context, @DrawableRes int vectorDrawableResourceId) {
+////        Drawable background = ContextCompat.getDrawable(context, R.drawable.pin_point);
+////        background.setBounds(0, 0, background.getIntrinsicWidth(), background.getIntrinsicHeight());
+//        Drawable vectorDrawable = ContextCompat.getDrawable(context, vectorDrawableResourceId);
+//        vectorDrawable.setBounds(0, 0, vectorDrawable.getIntrinsicWidth() + 0, vectorDrawable.getIntrinsicHeight() + 0);
+//        Bitmap bitmap = Bitmap.createBitmap(background.getIntrinsicWidth(), background.getIntrinsicHeight(), Bitmap.Config.ARGB_8888);
+//        Canvas canvas = new Canvas(bitmap);
+//        background.draw(canvas);
+//        vectorDrawable.draw(canvas);
+//        return BitmapDescriptorFactory.fromBitmap(bitmap);
+//    }
 }
